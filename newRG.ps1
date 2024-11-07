741138bc01e6a2d9b4dd29b012ec2aba00f53412
Anubhav <anubhavcp@gmail.com>
2024-11-07 14:08:06 +0530

    Added error action for new rg creation to stop the script execution in case changing az context to right subscription failed

--- newRG.ps1
+++ newRG.ps1
@@ -6,6 +6,6 @@ $rgTags = @{""="";"x"=""}
 $currentSubscription = (Get-AzContext).Subscription.Name.ToString()
 if ($currentSubscription -ne $rgSubscription){
   #
-  Set-AzContext -Subscription $rgSubscription
+  Set-AzContext -Subscription $rgSubscription -ErrorAction Stop
 }
 New-AzResourceGroup -Name $rgname -Location $rgLocation -Tag $rgTags
