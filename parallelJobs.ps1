162d6b4e158f73bf6725fed40a726b02c86f43b0
Anubhav <anubhavcp@gmail.com>
2024-11-07 14:13:32 +0530

    working

--- parallelJobs.ps1
+++ parallelJobs.ps1
@@ -9,20 +9,21 @@ $task = {
 
     Start-Sleep -Seconds 11
     Write-Error "This is an error to be printed"
-    Get-Item "C:\NonExistentFile1.txt" -ErrorAction Stop -ErrorVariable $Global:jobErrors
+    Get-Item "C:\NonExistentFile1.txt" -ErrorAction Stop
     Write-Host "Finished Job for" $vm.Name
 }
 #
 $Global:jobs = @()
 $Global:jobCounter = 0
 $Global:totalJobs = 0
-$Global:jobErrors = @()
+$Global:jobErrors = ""
+$Global:errorFile = @()
 
 $Global:totalJobs = ($vms | Measure-Object).Count
 $vms | ForEach-Object {
     if ( $jobCounter -lt $maxJobCount) {
         Write-Host Starting Job of $_.Name
-        $job = Start-Job -Name $_.Name -ScriptBlock $task -ArgumentList $_ 
+        $job = Start-Job -Name $_.Name -ScriptBlock $task -ArgumentList $_
         $Global:jobs += $job
         $Global:jobCounter++
     } 
@@ -33,7 +34,7 @@ while ($true) {
     #Write-Host Current Job is #$currentlyRunningJobs
     if ((($currentlyRunningJobs | Measure-Object).Count -lt $maxJobCount) -and (($jobCounter) -lt $Global:totalJobs)) {
         Write-Host Starting Job of $vms[$Global:jobCounter].Name
-        $job = Start-Job -Name $vms[$Global:jobCounter].Name -ScriptBlock $task -ArgumentList $vms[$Global:jobCounter]
+        $job = Start-Job -Name $vms[$Global:jobCounter].Name -ScriptBlock $task -ArgumentList $vms[$Global:jobCounter]  
         $Global:jobs += $job
         $Global:jobCounter++
     }
@@ -54,8 +55,17 @@ while ($true) {
                 $failedJobs | Select-Object Id, Name, State, HasMoreData | Format-Table -AutoSize -RepeatHeader
                 $failedJobs | ForEach-Object {
                     ($_ | Select-Object Id, Name, State, HasMoreData | Format-Table -AutoSize -HideTableHeaders)
-                    Write-Host (Receive-Job -Job $_) 
+                    $errorDetails = (Receive-Job -Job $_ -Keep) 
+                    Write-Host $errorDetails
+                    # code to export a csv file containing failed job name and error details
+                    # and this is still pending 
+                    # $errorInfo = [PSCustomObject]@{
+                    #     JobName = $_.Name;
+                    #     Error   = ($_.Error | ForEach-Object { $_.Exception.Message }) -join "; ";
+                    # }
+                    # $Global:errorFile += $errorInfo
                 }
+                $Global:errorFile | Export-Csv -Path "./failedJobs.csv" -NoTypeInformation -Force
             }
             Write-Host All Jobs Finished
             break
