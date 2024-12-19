1ab2286c6809395ca4f792703180394e6d7c8b88
Anubhav <anubhavcp@gmail.com>
2024-12-19 15:32:43 +0530

    modified additionalwork file

--- additionalWorkandPatch.ps1
+++ additionalWorkandPatch.ps1
@@ -12,24 +12,34 @@ $amaExt = {
 
 $dcrAssociation = {
     param(
-        $vmDcr
+        $vmId
     )
     $dcrName = ""
-    Set-AzContext -SubscriptionId ($vmDcr.Id -split("/"))[2]
+    Set-AzContext -SubscriptionId ($vmId -split("/"))[2]
     $dcr = Get-AzDataCollectionRule | where Name -Like $dcrName
     if ($dcr) {
-        New-AzDataCollectionRuleAssociation -AssociationName ($dcr.Name + "-association") -DataCollectionRuleId $dcr.Id -ResourceUri $vmDcr.Id
+        New-AzDataCollectionRuleAssociation -AssociationName ($dcr.Name + "-association") -DataCollectionRuleId $dcr.Id -ResourceUri $vmId
     }
 }
 
 $patchMode = {
     param(
-        $vmPatMode
+        $resId
     )
-    Set-AzContext -SubscriptionId ($vmPatMode.Id -split("/"))[2]
-    $vmPatMode.OSProfile.WindowsConfiguration.PatchSettings.patchMode = "AutomaticByPlatform"
-    $vmPatMode.OSProfile.WindowsConfiguration.PatchSettings.AutomaticByPlatformSettings = @{"bypassPlatformSafetyChecksOnUserSchedule" = $true}
-    Update-AzVM -ResourceGroupName $vmPatMode.ResourceGroupName -VM $vmPatMode
+    Set-AzContext -SubscriptionId ($resId -split("/"))[2]
+    $Vm = Get-AzVM -ResourceId $resId
+    $Vm.OSProfile.WindowsConfiguration.PatchSettings.patchMode = "AutomaticByPlatform"
+    $Vm.OSProfile.WindowsConfiguration.PatchSettings.AutomaticByPlatformSettings = @{"bypassPlatformSafetyChecksOnUserSchedule" = $true}
+    Update-AzVM -ResourceGroupName $Vm.ResourceGroupName -VM $Vm
+}
+
+$sysIdentity = {
+    param(
+        $resId
+    )
+    Set-AzContext -SubscriptionId ($resId -split("/"))[2]
+    $vmSysIdentity = Get-AzVM -ResourceId $resId
+    Update-AzVM -ResourceGroupName $vmSysIdentity.ResourceGroupName -VM $vmSysIdentity -IdentityType SystemAssigned
 }
 
 $vmname = $vmName.trim()
@@ -42,8 +52,10 @@ $resIdCount = ($resId | Measure-Object).Count
 if ($resIdCount -gt 0 -and $resIdCount -lt 2) {
     $AzVm = Get-AzVM -ResourceId $resId
     Start-Job -Name ($AzVm.Name + "-amaExtension") -ScriptBlock $amaExt -ArgumentList $AzVm
-    Start-Job -Name ($AzVm.Name + "-dcrAssociation") -ScriptBlock $dcrAssociation -ArgumentList $AzVm
-    Start-Job -Name ($AzVm.Name + "-patchMode") -ScriptBlock $patchMode -ArgumentList $AzVm
+    Start-Job -Name ($AzVm.Name + "-sysIdentity") -ScriptBlock $sysIdentity -ArgumentList $resId
+    Start-Job -Name ($AzVm.Name + "-dcrAssociation") -ScriptBlock $dcrAssociation -ArgumentList $resId
+    Start-Job -Name ($AzVm.Name + "-patchMode") -ScriptBlock $patchMode -ArgumentList $resId
+    
 } else {
     Write-Error $vmname Not found
 }
