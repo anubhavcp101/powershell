b1754025babb54e10acd1a80e6f9c190522df46e
Anubhav <anubhavcp@gmail.com>
2025-03-04 16:43:42 +0530

    added page file

---
+++ configPagefile.ps1
@@ -0,0 +1,19 @@
+#
+$RAM = (Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory / 1MB
+$MinSize = [math]::Round(2 * $RAM)
+$TDriveSize = (Get-PSDrive -Name T).Used / 1MB + (Get-PSDrive -Name T).Free / 1MB
+$MaxSize = [math]::Round(0.84 * $TDriveSize)
+$PageFile = Get-WmiObject Win32_PageFileSetting
+$PageFile.InitialSize = $MinSize
+#
+$PageFile.MaximumSize = $MaxSize
+$PageFile.Name = "T:\pagefile.sys"
+try {
+    $PageFile.Put()
+    #
+    Write-Host "Page file successfully configured on T:\pagefile.sys"
+} catch {
+    Write-Error "Error configuring page file: $($_.Exception.Message)"
+}
+Write-Host "A system reboot is required for these changes to take effect. So, rebooting it."
+shutdown /r 
