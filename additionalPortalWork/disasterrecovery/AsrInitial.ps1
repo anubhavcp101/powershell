51ba93f3360883f8ff29d9b64f18c5788d463f1f
Anubhav <anubhavcp@gmail.com>
2025-06-09 18:33:48 +0530

    added asr file

---
+++ additionalPortalWork/disasterrecovery/AsrInitial.ps1
@@ -0,0 +1,119 @@
+#
+$vaultSubscription = ""
+$vaultName = ""
+$replicationPolicy = ""
+$primaryRegion = "" # "East US 3" or "West US 2"
+$secondaryRegion = "" # "East US 3" or "West US 2"
+
+#
+function WaitForAsrJob {
+    param (
+        $TempAsrJob
+    )
+    #
+    $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempAsrJob
+    while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")) {
+        Start-Sleep -Seconds 20
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+    }
+    return $TempAsrJob.StateDescription
+}
+Set-AzContext -Subscription $vaultSubscription
+$vault = Get-AzRecoveryServicesVault -Name $vaultName 
+Set-AzRecoveryServicesAsrVaultContext -Vault $vault
+
+#
+$createdContainer = $false
+$primaryFabric = Get-AzRecoveryServicesAsrFabric | Where-Object { $_.FriendlyName -like $primaryRegion }
+
+if (-not $primaryFabric) {
+    $TempASRJob = New-AzRecoveryServicesAsrFabric -Azure -Name "asr-a2a-primary-$($primaryRegion.tolower().replace(' ',''))" -Location $primaryRegion
+    WaitForAsrJob -TempAsrJob $TempASRJob
+    $primaryfab = Get-AzRecoveryServicesAsrFabric -Name "asr-a2a-primary-$($primaryRegion.tolower().replace(' ',''))"
+
+    $createdContainer = $true
+    $TempASRJob = New-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-primary-$($primaryRegion.tolower().replace(' ',''))-container" -InputObject $primaryfab
+    WaitForAsrJob -TempAsrJob $TempASRJob
+
+    $primaryContainer = Get-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-primary-$($primaryRegion.tolower().replace(' ',''))-container" -Fabric $primaryfab
+
+}
+
+$secondaryFabric = Get-AzRecoveryServicesAsrFabric | Where-Object { $_.FriendlyName -like $secondaryRegion }
+
+if (-not $secondaryFabric) {
+    $TempASRJob = New-AzRecoveryServicesAsrFabric -Azure -Name "asr-a2a-secondary-$($secondaryRegion.tolower().replace(' ',''))" -Location $secondaryRegion
+    WaitForAsrJob -TempAsrJob $TempASRJob
+    $secondaryfab = Get-AzRecoveryServicesAsrFabric -Name "asr-a2a-secondary-$($secondaryRegion.tolower().replace(' ',''))"
+
+    $createdContainer = $true
+    $TempASRJob = New-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-secondary-$($secondaryRegion.tolower().replace(' ',''))-container" -InputObject $secondaryfab
+    WaitForAsrJob -TempAsrJob $TempASRJob
+    $secondaryContainer = Get-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-secondary-$($secondaryRegion.tolower().replace(' ',''))-container" -Fabric $secondaryfab
+
+}
+
+$ReplicationPolicy = Get-AzRecoveryServicesAsrPolicy -Name $replicationPolicy
+
+if (-not $replicationPolicy) {
+    $TempASRJob = New-AzRecoveryServicesAsrPolicy -AzureToAzure -Name $replicationPolicy -RecoveryPointRetentionInHours 24 -ApplicationConsistentSnapshotFrequencyInHours 3
+    
+    WaitForAsrJob -TempAsrJob $TempASRJob
+    $ReplicationPolicy = Get-AzRecoveryServicesAsrPolicy -Name $replicationPolicy
+}
+
+$primaryContainer = (Get-AzRecoveryServicesAsrProtectionContainer -Fabric $primaryfab)[0]
+if (-not $primaryContainer) {
+    $createdContainer = $true
+    $TempASRJob = New-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-primary-$($primaryRegion.tolower().replace(' ',''))-container" -InputObject $primaryfab
+    WaitForAsrJob -TempAsrJob $TempASRJob
+
+    $primaryContainer = Get-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-primary-$($primaryRegion.tolower().replace(' ',''))-container" -Fabric $primaryfab
+
+}
+
+$secondaryContainer = (Get-AzRecoveryServicesAsrProtectionContainer -Fabric $secondaryfab)[0]
+if (-not $secondaryContainer) {
+    $createdContainer = $true
+    $TempASRJob = New-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-secondary-$($secondaryRegion.tolower().replace(' ',''))-container" -InputObject $secondaryfab
+    WaitForAsrJob -TempAsrJob $TempASRJob
+    $secondaryContainer = Get-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-secondary-$($secondaryRegion.tolower().replace(' ',''))-container" -Fabric $secondaryfab
+
+}
+
+if ($createdContainer) {
+
+    $TempASRJob = New-AzRecoveryServicesAsrProtectionContainerMapping -Name "$($primaryRegion.tolower().replace(' ',''))-$($secondaryRegion.tolower().replace(' ',''))-mapping" `
+        -Policy $replicationPolicy -PrimaryProtectionContainer $primaryContainer -RecoveryProtectionContainer $secondaryContainer
+
+    WaitForAsrJob -TempAsrJob $TempASRJob
+
+    $TempASRJob = New-AzRecoveryServicesAsrProtectionContainerMapping -Name "$($secondaryRegion.tolower().replace(' ',''))-$($primaryRegion.tolower().replace(' ',''))-mapping" `
+        -Policy $replicationPolicy -PrimaryProtectionContainer $secondaryContainer -RecoveryProtectionContainer $primaryContainer
+
+    WaitForAsrJob -TempAsrJob $TempASRJob
+}
+
+
+$primaryContainerMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $protContainer | where PolicyFriendlyName -eq $policyName | where SourceFabricFriendlyName -eq $primaryRegion | where TargetFabricFriendlyName -eq $secondaryRegion
+if (-not $primaryContainerMapping) {
+    $TempASRJob = New-AzRecoveryServicesAsrProtectionContainerMapping -Name "$($primaryRegion.tolower().replace(' ',''))-$($secondaryRegion.tolower().replace(' ',''))-mapping" `
+        -Policy $replicationPolicy -PrimaryProtectionContainer $primaryContainer -RecoveryProtectionContainer $secondaryContainer
+    
+    WaitForAsrJob -TempAsrJob $TempASRJob
+    $primaryContainerMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $protContainer | where PolicyFriendlyName -eq $policyName | where SourceFabricFriendlyName -eq $primaryRegion | where TargetFabricFriendlyName -eq $secondaryRegion
+
+}
+
+$secondaryContainerMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $protContainer | where PolicyFriendlyName -eq $replicationPolicy | where SourceFabricFriendlyName -eq $secondaryRegion | where TargetFabricFriendlyName -eq $primaryRegion
+
+if (-not $secondaryContainerMapping) {
+    $TempASRJob = New-AzRecoveryServicesAsrProtectionContainerMapping -Name "$($secondaryRegion.tolower().replace(' ',''))-$($primaryRegion.tolower().replace(' ',''))-mapping" `
+        -Policy $replicationPolicy -PrimaryProtectionContainer $secondaryContainer -RecoveryProtectionContainer $primaryContainer
+    
+    WaitForAsrJob -TempAsrJob $TempASRJob
+    $primaryContainerMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $protContainer | where PolicyFriendlyName -eq $policyName | where SourceFabricFriendlyName -eq $primaryRegion | where TargetFabricFriendlyName -eq $secondaryRegion
+
+}
+
+# Create Network Mapping 
--- additionalPortalWork/disasterrecovery/configureAsr.ps1
+++ additionalPortalWork/disasterrecovery/configureAsr.ps1
@@ -163,3 +163,114 @@ function ConfigureAsr {
     Write-Output $TempASRJob.State
 
 }
+
+# ------- Further Working --------------------
+
+if ($primaryRegion -eq "westus") {
+    $primaryLocation = "West US"
+}
+elseif ($primaryRegion -eq "westus3") {
+    $primaryLocation = "West US 3"
+}
+else {
+    $primaryLocation = $primaryRegion
+}
+$secondaryRegion = "East US 3"
+
+Set-AzContext -Subscription $vaultSubscription
+$vault = Get-AzRecoveryServicesVault -Name $vaultName 
+Set-AzRecoveryServicesAsrVaultContext -Vault $vault
+$fabric = Get-AzRecoveryServicesAsrFabric | Where-Object { $_.FriendlyName -like $primaryLocation } # "East US" or "West US 3"
+
+# $container = (Get-AzRecoveryServicesAsrProtectionContainer -Fabric $fabric | Where-Object { (($_.FabricFriendlyName -like $primaryLocation) -and ($_.AvailablePolicies.FriendlyName -contains "My-Policy")) })[0]
+
+$containers = Get-AzRecoveryServicesAsrProtectionContainer -Fabric $fabric | Where-Object { ($_.FabricFriendlyName -like $primaryLocation) }
+$foundContainer = $false
+$rightContainer = $null
+$foundPrimaryToRecoveryMapping = $false
+$foundRecoveryToPrimaryMapping = $false
+
+foreach ($container in $containers) {
+    foreach ($mapping in $container.ProtectionContainerMappings) {
+        if (($mapping.SourceFabricFriendlyName -like $primaryLocation) -and ($mapping.TargetFabricFriendlyName -like $secondaryRegion)) {
+            $foundPrimaryToRecoveryMapping = $true
+        }
+        elseif (($mapping.SourceFabricFriendlyName -like $secondaryRegion) -and ($mapping.TargetFabricFriendlyName -like $primaryLocation)) {
+            $foundRecoveryToPrimaryMapping = $true
+        }
+    } 
+    if ($foundPrimaryToRecoveryMapping -and $foundRecoveryToPrimaryMapping) {
+        $foundContainer = $true
+        $rightContainer = $container
+        break 
+    } 
+}
+
+if ( -not $foundContainer) {
+    Write-Error "Didn't find any container with right mappings"
+}
+
+
+$primaryContainerMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $rightContainer | where PolicyFriendlyName -eq $policyName | where SourceFabricFriendlyName -eq $primaryLocation
+
+# ---- Further Working -----------
+
+$replicationPolicy = ""
+$createdContainer = $false
+$primaryFabric = Get-AzRecoveryServicesAsrFabric | Where-Object { $_.FriendlyName -like $primaryLocation }
+
+if (-not $primaryFabric) {
+    New-AzRecoveryServicesAsrFabric -Azure -Name "asr-a2a-primary-$($primaryLocation.tolower().replace(' ',''))" -Location $primaryLocation
+    $primaryfab = Get-AzRecoveryServicesAsrFabric -Name "asr-a2a-primary-$($primaryLocation.tolower().replace(' ',''))"
+
+    $createdContainer = $true
+    New-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-primary-$($primaryLocation.tolower().replace(' ',''))-container" -InputObject $primaryfab
+    $primaryContainer = Get-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-primary-$($primaryRegion.tolower().replace(' ',''))-container" -Fabric $primaryfab
+
+}
+
+$secondaryFabric = Get-AzRecoveryServicesAsrFabric | Where-Object { $_.FriendlyName -like $secondaryRegion }
+
+if (-not $secondaryFabric) {
+    New-AzRecoveryServicesAsrFabric -Azure -Name "asr-a2a-secondary-$($secondaryRegion.tolower().replace(' ',''))" -Location $secondaryRegion
+    $secondaryfab = Get-AzRecoveryServicesAsrFabric -Name "asr-a2a-secondary-$($secondaryRegion.tolower().replace(' ',''))"
+
+    $createdContainer = $true
+    New-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-secondary-$($secondaryRegion.tolower().replace(' ',''))-container" -InputObject $secondaryfab
+    $secondaryContainer = Get-AzRecoveryServicesAsrProtectionContainer -Name "asr-a2a-secondary-$($secondaryRegion.tolower().replace(' ',''))-container" -Fabric $secondaryfab
+
+}
+
+$ReplicationPolicy = Get-AzRecoveryServicesAsrPolicy -Name $replicationPolicy
+
+if ( -not $replicationPolicy) {
+    New-AzRecoveryServicesAsrPolicy -AzureToAzure -Name $replicationPolicy -RecoveryPointRetentionInHours 24 -ApplicationConsistentSnapshotFrequencyInHours 3
+    $ReplicationPolicy = Get-AzRecoveryServicesAsrPolicy -Name $replicationPolicy
+}
+
+if ($createdContainer) {
+    # create container mappings
+
+    New-AzRecoveryServicesAsrProtectionContainerMapping -Name "$($primaryRegion.tolower().replace(' ',''))-$($secondaryRegion.tolower().replace(' ',''))-mapping" `
+        -Policy $replicationPolicy -PrimaryProtectionContainer $primaryContainer -RecoveryProtectionContainer $secondaryContainer
+
+    New-AzRecoveryServicesAsrProtectionContainerMapping -Name "$($secondaryRegion.tolower().replace(' ',''))-$($primaryRegion.tolower().replace(' ',''))-mapping" `
+        -Policy $replicationPolicy -PrimaryProtectionContainer $secondaryContainer -RecoveryProtectionContainer $primaryContainer
+}
+
+$primaryContainerMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $protContainer | where PolicyFriendlyName -eq $policyName | where SourceFabricFriendlyName -eq $primaryLocation | where TargetFabricFriendlyName -eq $secondaryRegion
+if (-not $primaryContainerMapping) {
+    New-AzRecoveryServicesAsrProtectionContainerMapping -Name "$($primaryRegion.tolower().replace(' ',''))-$($secondaryRegion.tolower().replace(' ',''))-mapping" `
+        -Policy $replicationPolicy -PrimaryProtectionContainer $primaryContainer -RecoveryProtectionContainer $secondaryContainer
+    $primaryContainerMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $protContainer | where PolicyFriendlyName -eq $policyName | where SourceFabricFriendlyName -eq $primaryLocation | where TargetFabricFriendlyName -eq $secondaryRegion
+
+}
+
+$secondaryContainerMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $protContainer | where PolicyFriendlyName -eq $replicationPolicy | where SourceFabricFriendlyName -eq $secondaryRegion | where TargetFabricFriendlyName -eq $primaryLocation
+
+if (-not $secondaryContainerMapping) {
+    New-AzRecoveryServicesAsrProtectionContainerMapping -Name "$($secondaryRegion.tolower().replace(' ',''))-$($primaryLocation.tolower().replace(' ',''))-mapping" `
+        -Policy $replicationPolicy -PrimaryProtectionContainer $secondaryContainer -RecoveryProtectionContainer $primaryContainer
+    $primaryContainerMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $protContainer | where PolicyFriendlyName -eq $policyName | where SourceFabricFriendlyName -eq $primaryLocation | where TargetFabricFriendlyName -eq $secondaryRegion
+
+}
