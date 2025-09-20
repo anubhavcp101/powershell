bc62defd869b92e6d4bcc139f2cd51bff2009ce9
Anubhav <anubhavcp@gmail.com>
2025-09-20 06:24:17 +0530

    updated extension files and updated jobs files

--- Parallel-Jobs/parallelJobs2.ps1
+++ Parallel-Jobs/parallelJobs2.ps1
@@ -12,33 +12,6 @@ $task = {
         # Use $PSDefaultParameterValues['command:parameter'] = $value to pass value to common parameters
         Set-Location $wrkdir 
 
-        $vmname = $vm.Name.trim()
-        $resId = (Search-AzGraph -Query ("resources | where type == ""microsoft.compute/virtualmachines"" | where name like """ + $vmname + """ | project id") -UseTenantScope).id; write $resId;
-        $subsId = (Search-AzGraph -Query ("resources | where type == ""microsoft.compute/virtualmachines"" | where name like """ + $vmname + """ | project subscriptionId") -UseTenantScope).subscriptionId; write $subsId;
-        $currentSubscriptionId = (Get-AzContext).Subscription.Id.ToString()
-        #
-        if ($currentSubscriptionId -ne $subsId) { Set-AzContext -SubscriptionId $subsId -ErrorAction Stop }
-        $resIdCount = ($resId | Measure-Object).Count 
-        if ($resIdCount -gt 0 -and $resIdCount -lt 2) {
-            $AzVm = Get-AzVM -ResourceId $resId
-
-            if ($AzVm.StorageProfile.OsDisk.OsType -eq "Windows") {
-                $extension = Get-AzVMExtension -ResourceGroupName $AzVm.ResourceGroupName -VMName $AzVm.Name -Name "AzureMonitorWindowsAgent" -Status
-            } else {
-                $extension = Get-AzVMExtension -ResourceGroupName $AzVm.ResourceGroupName -VMName $azvm.Name -Name "AzureMonitorLinuxAgent" -Status
-            }
-            $outFolderPath = Get-ChildItem -Path . -Directory | Where-Object {$.BaseName -like "Output-*"} | Sort-Object -Property CreationTime -Descending | Select-Object -First 1 | Select-Object -ExpandProperty FullName
-            $outputPath = "$($outFolderPath)/$($AzVm.Name)-$(Get-Date -Format 'dd-MM-yyyy-hh-mm').csv"
-            "Date,Name,Status,Message" | Out-File -FilePath $outputPath -Append -Force
-            "$(Get-Date -Format 'dd-MM-yyyyThh-mm'),$($AzVm.Name),$($extension.Statuses.DisplayStatus),$($extension.Statuses.Message)" | Out-File -FilePath $outputPath -Append -Force
-
-
-        }
-        else {
-            Write-Error $vmname Not found
-        }
-
-
         Start-Sleep -Seconds 11
         Write-Error "This is an error to be printed"
         Get-Item "C:\NonExistentFile2.txt" -ErrorAction Stop
@@ -54,14 +27,9 @@ $Global:jobCounter = 0
 $Global:totalJobs = 0
 $Global:jobErrors = ""
 $Global:errorFile = @()
-Set-Location $PSScriptRoot
 $wrkdir = $PSScriptRoot
+Set-Location $PSScriptRoot
 $vms = Import-Csv -Path $filePath #-Header "Name"
-
-$outFolderName = "Output-$(Get-Date -Format 'dd-MM-yyyy-hh-mm')"
-
-$newFolder = New-Item -Path . -Name $outFolderName -ItemType Directory -Force | Out-Null
-Write-Output "The Output Folder is at : $($newFolder.FullName)"
 
 
 $Global:totalJobs = ($vms | Measure-Object).Count
@@ -132,13 +100,3 @@ while ($true) {
         Start-Sleep -Seconds 30
     }
 }
-
-Set-Location $newFolder.FullName
-$allFiles = Import-Csv -Path (Get-ChildItem -Path ".\*.csv").FullName 
-$combinedFile = "00-CombinedFile-$(Get-Date -Format 'dd-MM-yyyy-hh-mm').csv"
-Set-Location -Path (Split-Path -Path (Get-Location) -Parent)
-$allFiles | Export-Csv -Path ".\$combinedFile" -NoTypeInformation -Force
-$finalFile = Get-ChildItem -Path ".\$($combinedFile)"
-Write-Output "The Generated File is $($combinedFile)"
-Write-Output "It is at $($finalFile.FullName)"
-Compress-Archive -Path $newFolder.FullName -DestinationPath ".\$($newFolder.BaseName).zip" -Force
