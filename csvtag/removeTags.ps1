3eb03d32d8b8ada3487773708097e62a6bd769cb
Anubhav <anubhavcp@gmail.com>
2026-04-17 13:28:49 +0530

    added the tags files

---
+++ csvtag/removeTags.ps1
@@ -0,0 +1,12 @@
+#
+$TagFilepath = "" # csv file for tags with column Key and Value
+$resourceFilepath = "" # csv file for resources with columns ResourceId
+$csvtags = Import-Csv -Path $TagFilepath
+$tags=@{}
+$csvtags | ForEach-Object { $tags[$_.Key] = $_.Value}
+$resources = Import-Csv $resourceFilepath
+#
+foreach( $res in $resources) {
+    Set-AzContext -Subscription (($res.ResourceId -split("/")[2]))
+    Update-AzTag -Operation Delete -ResourceId $res.ResourceId -Tag $tags
+}
