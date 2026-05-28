e9ad74aa548cf2773f6fca106df635587bda00a5
Anubhav <anubhavcp@gmail.com>
2026-05-28 08:27:34 +0530

    added the update scanner file

---
+++ ft2-Parallel-Jobs/Working/updateScanner.ps1
@@ -0,0 +1,19 @@
+#
+$session = New-Object -ComObject Microsoft.Update.Session 
+$searcher = $session.CreateUpdateSearcher()
+
+$searchResult = $searcher.Search("IsInstalled=0 and IsHidden=0")
+$updates = foreach ($update in $searchResult.Updates) {
+
+    #
+    [PSCustomObject]@{
+        Title = $update.Title
+        KB = $update.KBArticleIDs -join ","
+        Classification = @(foreach($cate in $update.Categories){ $cate.Name })
+        #
+        Severity = $update.MsrcSeverity
+        RebootNeeded = $update.RebootRequired
+        EulaAccepted = $update.EulaAccepted
+    }
+}
+$updates
