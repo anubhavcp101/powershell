e22e5e820b7a312b3f9b004f789739e127e5161b
Anubhav <anubhavcp@gmail.com>
2026-06-23 13:14:07 +0530

    controller-target runbook added

---
+++ ft2-Parallel-Jobs/Working/autoSchedule/controller.ps1
@@ -0,0 +1,40 @@
+#
+$subscription = ''
+$rg = ''
+$automationAccountName = ''
+$targetRunbook = ''
+
+# its adding the timezone offset to the time provided here so adjust it accordingly
+#
+$initDate = Get-Date -Date (Get-Date).Date -Month (Get-Date).Month -Year (Get-Date).Year -Hour 16 -Minute 0 -Second 0
+$dates = @(
+    ($initDate).AddDays(3),
+    ($initDate).AddDays(5),
+    #
+    ($initDate).AddDays(7),
+    ($initDate).AddDays(9),
+    ($initDate).AddDays(11)
+)
+#
+Set-AzContext -Subscription $subscription
+
+$counter = 0
+foreach ( $date in $dates) {
+    $scheduleName = "OneTimeSchedule-$($initDate.Month)-$($initDate.Year)-$($counter)"
+    $counter++
+    New-AzAutomationSchedule -Name $scheduleName -StartTime $date -TimeZone 'India Standard Time' -ResourceGroupName $rg -AutomationAccountName $automationAccountName -OneTime
+
+    Register-AzAutomationScheduledRunbook -ScheduleName $scheduleName -RunbookName $targetRunbook -ResourceGroupName $rg -AutomationAccountName $automationAccountName
+
+}
+
+<#
+# to get automation schedule in past time
+
+get-azAutomationSchedule -ResourceGroupName $rg -AutomationAccountName $automationAccountName | Where-Object { $_.Name -match 'OneTimeSchedule-\d{1}-\d{4}-\d{1}' } | Where-Object { $_.NextRun.DateTime -lt (Get-Date) }
+
+get-azAutomationSchedule -ResourceGroupName $rg -AutomationAccountName $automationAccountName | Where-Object { $_.Name -match 'OneTimeSchedule-\d{1}-\d{4}-\d{1}' } | Where-Object { $_.NextRun.DateTime -lt (Get-Date) } | Where-Object { $_.Frequency -eq 'Onetime' } | measure
+
+# and removing them
+get-azAutomationSchedule -ResourceGroupName $rg -AutomationAccountName $automationAccountName | Where-Object { $_.Name -match 'OneTimeSchedule-\d{1}-\d{4}-\d{1}' } | Where-Object { $_.NextRun.DateTime -gt (Get-Date) } | Where-Object { $_.Frequency -eq 'Onetime' } | Remove-AzAutomationSchedule -Force
+#>
