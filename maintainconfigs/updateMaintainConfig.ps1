eaa509df25f75f832c0b8d71bc8fd71b11208296
Anubhav <anubhavcp@gmail.com>
2026-07-26 15:56:13 +0530

    main-config updated

--- maintainconfigs/updateMaintainConfig.ps1
+++ maintainconfigs/updateMaintainConfig.ps1
@@ -6,6 +6,25 @@ $subscription = ""
 Set-AzContext -Subscription $subscription
 $maintainConfig = Get-AzMaintenanceConfiguration -ResourceGroupName $rg -Name $maintainConfigName
 #
-$kbs | ForEach-Object { $mc.WindowParameterKbNumberToInclude.add($_) } 
+$kbs | ForEach-Object { $maintainConfig.WindowParameterKbNumberToInclude.add($_) } 
 # $maintainConfig.WindowParameterKbNumberToInclude.Add("")
 Update-AzMaintenanceConfiguration -ResourceGroupName $rg -Name $maintainConfigName -Configuration $maintainConfig
+# Validate
+#
+$attempt = 0 
+do {
+    # try to update
+    $attempt++
+    $kbs | ForEach-Object { $maintainConfig.WindowParameterKbNumberToInclude.add($_) } 
+    Update-AzMaintenanceConfiguration -ResourceGroupName $rg -Name $maintainConfigName -Configuration $maintainConfig
+    $res = Compare-Object -ReferenceObject $kbs -DifferenceObject @($maintainConfig.WindowParameterKbNumberToInclude)
+} while (
+    ($null -ne $res) -and ($attempt -lt 3)
+) 
+$res = Compare-Object -ReferenceObject $kbs -DifferenceObject @($maintainConfig.WindowParameterKbNumberToInclude)
+if ($null -eq $res) {
+    Write-Output "Successfully Updated"
+} else {
+    Write-Output "Please verify"
+    $res
+}
